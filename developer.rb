5cbf555bc6fece9f29540d0d58a98b435400c11d
koloss <in2deal@gmail.com>
2016-02-17 17:34:39 +0200

    Array version DEVELOPER

--- developer.rb
+++ developer.rb
@@ -1,13 +1,16 @@
 class Developer
 
+MAX_TASKS = 10
+
 	def initialize(name)
 		@name = name
-		@task_amount = 0
+		@task_list = Array.new()
 	end
 
 
 	def add_task(task)
-		puts "#{@name}: добавлена задача #{task}. Всего в списке задач: #{@task_amount += 1}"
+		@task_list.push(task)
+		puts "#{@name}: добавлена задача #{task}. Всего в списке задач: #{@task_list.length}"
 	end
 
 end
