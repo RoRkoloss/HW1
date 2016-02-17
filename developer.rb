decf369139e02ffd917ec2d3a4c31ff227e4e51c
koloss <in2deal@gmail.com>
2016-02-17 21:51:10 +0200

    add TASKS and Work

--- developer.rb
+++ developer.rb
@@ -1,4 +1,6 @@
 class Developer
+attr_reader :task_list
+#attr_writer :task_list
 
 MAX_TASKS = 10
 
@@ -13,9 +15,18 @@ MAX_TASKS = 10
 		puts "#{@name}: добавлена задача #{task}. Всего в списке задач: #{@task_list.length}"
 	end
 
+	def tasks()
+		
+		@task_list.each_with_index{|a, i| puts "#{i + 1}. #{a}"}
+
+				
+	end
+
 end
 
 dev = Developer.new ("Вася")
+dev
 dev.add_task("Полить кактус")
 dev.add_task("Полить морковку")
+dev.tasks
 
