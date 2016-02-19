9072bade1ae1933b1cfc4bd776957931fc7d7ea1
koloss <in2deal@gmail.com>
2016-02-19 11:05:33 +0200

    Last version

--- developer.rb
+++ developer.rb
@@ -1,6 +1,4 @@
 class Developer
-attr_reader :task_list
-#attr_writer :task_list
 
 MAX_TASKS = 10
 
@@ -18,12 +16,12 @@ MAX_TASKS = 10
     puts %Q{#{@name}: добавлена задача "#{task}". Всего в списке задач: #{@task_list.length}}
   end
 
-
+# prints the list of tasks 
   def tasks()	
     @task_list.each_with_index{|a, i| puts "#{i + 1}. #{a}"}
   end
 
-
+# prints and deletes work
   def work!
   	if @task_list.empty?	
   	  raise("Нечего делать!") 	
@@ -31,6 +29,7 @@ MAX_TASKS = 10
   	puts %Q{#{@name}: выполнена задача "#{@task_list.shift}". Осталось задач: #{task_list.length}}
   end
 
+# checks possibility to work
   def status
     case 
     when can_work?
@@ -66,6 +65,7 @@ class JuniorDeveloper < Developer
       puts %Q{#{@name}: добавлена задача "#{task}". Всего в списке задач: #{@task_list.length}} 
   end
 
+# prints and deletes work
 def work!
   	if @task_list.empty?	
   	  raise("Нечего делать!") 	
@@ -76,26 +76,56 @@ def work!
 end
 
 class SeniorDeveloper < Developer
-  MAX_TASKS = 15
+  
+MAX_TASKS = 15
 
+  def add_task(task)
+  	if @task_list.length == MAX_TASKS	
+  	  raise("Слишком много работы!")
+  	end
+    @task_list.push(task)
+    puts %Q{#{@name}: добавлена задача "#{task}". Всего в списке задач: #{@task_list.length}}
+  end
+
+# makes a choise
   def work!
+    a = Random.rand < 0.5
+    case a
+      when true
+      	several_tasks
+      else
+      	puts 'Что-то лень'
+    end	
+  end
+
+# does several tasks
+  def several_tasks
   	if @task_list.empty?	
   	  raise("Нечего делать!") 	
   	end
-  	puts %Q{#{@name}: выполнена задача "#{@task_list.shift}". Осталось задач: #{task_list.length}}
+  	  puts %Q{#{@name}: выполнена задача "#{@task_list.shift}". Осталось задач: #{task_list.length}}
+  	unless @task_list.empty?
+  	  puts %Q{#{@name}: выполнена задача "#{@task_list.shift}". Осталось задач: #{task_list.length}}	
+  	end
   end
-  
+
 end
 
 
-p dev = JuniorDeveloper.new("Вася")
-dev
+# class for a check of homework
+/class Check
+sdev = SeniorDeveloper.new("Senior")
+dev = Developer.new("Jun")
 5.times do |i| 
-	dev.add_task("Полить кактус")
+	dev.add_task("Kактус")
+	sdev.add_task("Морковка")
 end
 #dev.add_task("Полить морковку")
 dev.tasks 
-#dev.work!
-#dev.tasks
-dev.work!
-dev.status
+#sdev.work!
+#sdev.tasks
+#jdev.work!
+#jdev.tasks
+#sdev.status
+#jdev.status
+end/
