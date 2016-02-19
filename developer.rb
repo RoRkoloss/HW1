54f17047328b8feb8ca5fa351420fdb6836597ae
koloss <in2deal@gmail.com>
2016-02-19 08:43:37 +0200

    add "can_add_task?"

--- developer.rb
+++ developer.rb
@@ -4,29 +4,54 @@ attr_reader :task_list
 
 MAX_TASKS = 10
 
-	def initialize(name)
-		@name = name
-		@task_list = Array.new()
-	end
+  def initialize(name)
+    @name = name
+    @task_list = Array.new()
+  end
 
 
-	def add_task(task)
-		@task_list.push(task)
-		puts "#{@name}: добавлена задача #{task}. Всего в списке задач: #{@task_list.length}"
-	end
+  def add_task(task)
+  	if @task_list.length == MAX_TASKS	
+  	  raise("Слишком много работы!") 	
+  	end
+    @task_list.push(task)
+    puts %Q{#{@name}: добавлена задача "#{task}". Всего в списке задач: #{@task_list.length}}
+  end
 
-	def tasks()
-		
-		@task_list.each_with_index{|a, i| puts "#{i + 1}. #{a}"}
 
-				
-	end
+  def tasks()	
+    @task_list.each_with_index{|a, i| puts "#{i + 1}. #{a}"}
+  end
 
+
+  def work!
+  	if @task_list.empty?	
+  	  raise("Нечего делать!") 	
+  	end
+  	puts %Q{#{@name}: выполнена задача "#{@task_list.shift}". Осталось задач: #{task_list.length}}
+  end
+
+  def status
+    case 
+    when @task_list.empty?
+    	puts %Q{"свободен"}
+    when @task_list.length < MAX_TASKS
+    	puts %Q{"работаю"}
+    else
+    	puts %Q{"занят"}
+    end
+
+  end
 end
 
 dev = Developer.new ("Вася")
 dev
-dev.add_task("Полить кактус")
+9.times do |i| 
+	dev.add_task("Полить кактус")
+end
 dev.add_task("Полить морковку")
-dev.tasks
-
+dev.tasks 
+#dev.work!
+#dev.tasks
+#dev.work!
+dev.status
